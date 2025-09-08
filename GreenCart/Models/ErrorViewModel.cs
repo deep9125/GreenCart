7eca8c660da73c9e12b5d194cfa7a043ea3d95f0
deep9125 <rathoddeep711@gmail.com>
2025-09-08 22:38:52 +0530

    first commit

---
+++ GreenCart/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GreenCart.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
